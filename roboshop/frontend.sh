1979429b1f94f3e0eef1027c9da30b2c223ec3ce
pankaj <psingh@huronconsultinggroup.com>
2021-09-18 13:23:06 +0530

    added validation and fomated

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -5,7 +5,7 @@ LOG=/tmp/roboshop.log
 rm -f $LOG
 echo -e "installing Nginx\t\t..."
 yum install nginx -y &>>$LOG
-if[ $? == 0 ] then 
+if[ $? -eq 0 ]; then 
     echo "\t\e[32mdone\e[m"
 else
     "\t\e[32mFail\e[m"
