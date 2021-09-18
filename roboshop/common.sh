3cab1001278621534bd3b20c97eee696688cd067
pankaj <psingh@huronconsultinggroup.com>
2021-09-18 14:01:41 +0530

    added

---
+++ roboshop/common.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+LOG=/tmp/roboshop.log
+
+#if previously any log is there it will delte and genrate new, in case you want the history you can move it and store.
+rm -f $LOG
+STAT_CHECK(){
+    if [ $1 -eq 0 ]; 
+then 
+    echo -e "\e[32mDone\e[m"
+else
+    echo -e "\e[31mFail\e[m"
+    exit 1
+fi
+}
+
+USER_ID=$(id -u)
+if [ $? -ne 0 ]; then
+    echo -e "\e[31mYou shoud be root or sudo user to run the script\e[0m"
+    eixt 2
+fi
+
+PRINT()
+{
+    echo -n -e "$1\t\t... "
+}
--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
-LOG=/tmp/roboshop.log
 
-#if previously any log is there it will delte and genrate new, in case you want the history you can move it and store.
-rm -f $LOG
-
-STAT_CHECK(){
-    if [ $1 -eq 0 ]; 
-then 
-    echo -e "\e[32mDone\e[m"
-else
-    echo -e "\e[31mFail\e[m"
-    exit 1
-fi
-
-}
-echo -n -e "installing Nginx\t\t... "
+source common.sh
+PRINT "installing Nginx"
 yum install nginx -y &>>$LOG
 STAT_CHECK $?
 
-echo -n -e "Enabling Nginx\t\t\t... "
+PRINT "Enabling Nginx\t"
 systemctl enable nginx &>>$LOG 
 STAT_CHECK $?
 
-echo -n -e "Starting Nginx\t\t\t... "
+PRINT "Starting Nginx\t"
 systemctl start nginx &>>$LOG
 STAT_CHECK $?
