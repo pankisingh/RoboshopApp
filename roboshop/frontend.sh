87aa6614402fd49507f5b6ef5f193b99f67c64ee
pankaj <psingh@huronconsultinggroup.com>
2021-09-18 12:37:04 +0530

    std out put and err added.

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -4,10 +4,10 @@ LOG=/tmp/roboshop.log
 #if previously any log is there it will delte and genrate new, in case you want the history you can move it and store.
 rm -f $LOG
 echo -e "installing Nginx\t\t...\t\e[32mdone\e[m"
-yum install nginx -y >>$LOG
+yum install nginx -y &>$LOG
 
 echo "Enabling Nginx"
-systemctl enable nginx >>$LOG 
+systemctl enable nginx &>$LOG 
 
 echo "Starting Nginx"
-systemctl start nginx >>$LOG
+systemctl start nginx &>$LOG
