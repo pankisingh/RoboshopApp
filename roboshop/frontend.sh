e22d9bf6f84d323e18c710e267d24b11ae830f5f
pankaj <psingh@huronconsultinggroup.com>
2021-09-18 14:24:23 +0530

    added

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -5,10 +5,26 @@ PRINT "installing Nginx"
 yum install nginx -y &>>$LOG
 STAT_CHECK $?
 
+PRINT "Download Frontend"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+STAT_CHECK $?
+
+PRINT "Remove old HtDocs"
+cd /usr/share/nginx/html && rm -rf * &>>$LOG
+STAT_CHECK $?
+
+PRINT "Extract Frontend Archive"
+unzip /tmp/frontend.zip && mv frontend-main/* . && mv static/* . && rm -rf frontend-master static &>>$LOG
+STAT_CHECK $?
+
+PRINT "Update Roboshop Config"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT_CHECK $?
+
 PRINT "Enabling Nginx\t"
 systemctl enable nginx &>>$LOG 
 STAT_CHECK $?
 
 PRINT "Starting Nginx\t"
-systemctl start nginx &>>$LOG
+systemctl restart nginx &>>$LOG
 STAT_CHECK $?
