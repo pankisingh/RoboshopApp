88e7e0ef1b201ce79b632768f15d0abae0145946
pankaj <psingh@huronconsultinggroup.com>
2021-09-18 13:26:08 +0530

    a

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,9 +3,10 @@ LOG=/tmp/roboshop.log
 
 #if previously any log is there it will delte and genrate new, in case you want the history you can move it and store.
 rm -f $LOG
-echo -e "installing Nginx\t\t..."
+echo -n -e "installing Nginx\t\t..."
 yum install nginx -y &>>$LOG
-if[ $? -eq 0 ]; then 
+if[ $? -eq 0 ]; 
+then 
     echo "\t\e[32mdone\e[m"
 else
     "\t\e[32mFail\e[m"
