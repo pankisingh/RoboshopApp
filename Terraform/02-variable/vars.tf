2a6ff7d541c0f162e4e4196ccd383cea3d48bb47
pankaj <psingh@huronconsultinggroup.com>
2021-09-20 00:28:58 +0530

    added

--- Terraform/02-variable/vars.tf
+++ Terraform/02-variable/vars.tf
@@ -2,10 +2,17 @@ variable "Sample"{
     default = "Hello World"
 }
 
+variable "sample2"{}
+
+
+output "sample2" {
+    value = var.Sample2
+}
+
+
 output "sample" {
     value = var.Sample 
 }
-
 
 output "sample1" {
     value = "${var.Sample}" 
