499711f81fb94a0e6a4e7ef44f98d76da1edd339
pankaj <psingh@huronconsultinggroup.com>
2021-09-22 01:31:58 +0530

    added

--- Terraform/04-remote-state/sample.tf
+++ Terraform/04-remote-state/sample.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "sample" {
   ami                   = "ami-074df373d6bafa625"
-  instance_type         = "t3.micro"
+  instance_type         = "t2.micro"
 
   tags                  = {
-    Name                = "Sample"
+    Name                = "Sample2"
   }
 }
 
