d5604dc76e42b407a4496f1666138d94c4c1a058
pankaj <psingh@huronconsultinggroup.com>
2021-09-22 00:57:08 +0530

    added

--- Terraform/03-example1/ec2.tf
+++ Terraform/03-example1/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
     ami                        = "ami-074df373d6bafa625"
     instance_type              = "t2.micro"
-    svpc_security_group_ids    =  [aws_security_group.allow_ssh.id]
+    vpc_security_group_ids    =  [aws_security_group.allow_ssh.id]
     tags                       = {
                     Name     = "Sample"   
                 } 
