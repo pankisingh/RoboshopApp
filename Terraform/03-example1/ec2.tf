9afccfe8f7d7fdbe948bf9e67914774f6e33361f
pankaj <psingh@huronconsultinggroup.com>
2021-09-22 00:32:50 +0530

    added

--- Terraform/03-example1/ec2.tf
+++ Terraform/03-example1/ec2.tf
@@ -25,9 +25,8 @@ resource "aws_security_group" "allow_ssh" {
     {
       from_port        = 0
       to_port          = 0
-      protocol         = "-1"
+      protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
     }
   ]
 
