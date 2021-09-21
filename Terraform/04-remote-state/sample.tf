6935d59372b788b4f884a304693b5e00f1d171b4
pankaj <psingh@huronconsultinggroup.com>
2021-09-22 01:31:16 +0530

    added

---
+++ Terraform/04-remote-state/sample.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "sample" {
+  ami                   = "ami-074df373d6bafa625"
+  instance_type         = "t3.micro"
+
+  tags                  = {
+    Name                = "Sample"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform{
+    backend "s3" {
+        bucket = "terraform-state-b57"
+        key    = "sample/terraform-tfstate"
+        region = "us-east-1"
+
+    }
+}
