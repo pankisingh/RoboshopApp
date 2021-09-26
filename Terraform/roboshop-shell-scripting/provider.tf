6c96b491d9dec6868e3f3934bfe58b429baf785c
pankaj <psingh@huronconsultinggroup.com>
2021-09-26 12:44:29 +0530

    added

--- Terraform/roboshop-shell-scripting/provider.tf
+++ Terraform/roboshop-shell-scripting/provider.tf
@@ -8,6 +8,6 @@ terraform {
   bucket = "terraform-backup-057"
   key    = "roboshop/shell-scripting/terraform-tfstate"
   region =  "us-east-1"
-  dynamo_table = "terraform"
+  dynamodb_table = "terraform"
   }
 }
