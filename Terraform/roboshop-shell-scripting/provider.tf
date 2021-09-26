984c7a5467916450994f97a60be77bb49c341f2b
pankaj <psingh@huronconsultinggroup.com>
2021-09-26 12:57:41 +0530

    added

--- Terraform/roboshop-shell-scripting/provider.tf
+++ Terraform/roboshop-shell-scripting/provider.tf
@@ -2,16 +2,16 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "terraform-lock"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "LockID"
-attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+//resource "aws_dynamodb_table" "terraform_state_lock" {
+// name           = "terraform-lock"
+// read_capacity  = 5
+  //write_capacity = 5
+  //hash_key       = "LockID"
+//attribute {
+ //   name = "LockID"
+//    type = "S"
+//  }
+//}
 
 terraform {
   backend "s3"{
