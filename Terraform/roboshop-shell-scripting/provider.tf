392ea8287d4d2fd09a5fe0e414523d4365de04b1
pankaj <psingh@huronconsultinggroup.com>
2021-09-27 23:45:03 +0530

    added

--- Terraform/roboshop-shell-scripting/provider.tf
+++ Terraform/roboshop-shell-scripting/provider.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 terraform {
   backend "s3"{
-  bucket = "terraform-state-b57"
+  bucket = "terrafom-state-b04"
   key    = "roboshop/shell-scripting/terraform-tfstate"
   region =  "us-east-1"
   //dynamodb_table = "terraform-lock"
