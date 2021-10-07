1247654d817e697a6ec819d4b9779d7d683bff37
pankaj <psingh@huronconsultinggroup.com>
2021-10-07 23:29:59 +0530

    added

--- Terraform/for_each/vars.tf
+++ Terraform/for_each/vars.tf
@@ -1,8 +1,8 @@
 variable "filename" {
   type = set(string)
   default = [
-    "/c/Users/ag59900/Terraform_Test/hello1.txt",
-    "/c/Users/ag59900/Terraform_Test/hello2.txt",
-    "/c/Users/ag59900/Terraform_Test/hello3.txt",
+    "/home/centos/RoboshopApp/Terraform/for_each/hello1.txt",
+    "/home/centos/RoboshopApp/Terraform/for_each/hello2.txt",
+    "/home/centos/RoboshopApp/Terraform/for_each/hello3.txt",
   ]
 }
