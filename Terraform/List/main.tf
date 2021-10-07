73a452ec8ead3d7eccb7cb5865c05788b7b53241
pankaj <psingh@huronconsultinggroup.com>
2021-10-07 11:51:42 +0530

    added

--- Terraform/List/main.tf
+++ Terraform/List/main.tf
@@ -2,6 +2,6 @@ resource "random_pet" "my-list" {
  prefix  = var.prefix[0]
 }
 
-# output "random_pet" {
-#     value = var.prefix
-# }
+output "random_pet" {
+     value = "My value is = ${var.prefix}"
+ }
