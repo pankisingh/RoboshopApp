2574758c149d0022d36716bb0fd988eddddc09e2
pankaj <psingh@huronconsultinggroup.com>
2021-10-10 23:45:23 +0530

    added

--- ansible/terraform/ec2.tf
+++ ansible/terraform/ec2.tf
@@ -56,3 +56,19 @@ records    = [element(aws_spot_instance_request.cheap_worker.*.private_ip, count
 locals {
   Length = length(var.COMPONENTS)
 }
+
+//COMPONENTS = ["mysql", "mongodb", "rabbitmq", "redis", "cart", "catalogue", "user", "shipping", "payment", "frontend"]
+
+//resource "local_file" "inventory-file" {
+//  content     = "[FRONTEND]\n${aws_instance.instances.*.private_ip[9]}\n[PAYMENT]\n${aws_instance.instances.*.private_ip[8]}\n[SHIPPING]\n${aws_instance.instances.*.private_ip[7]}\n[USER]\n${aws_instance.instances.*.private_ip[6]}\n[CATALOGUE]\n${aws_instance.instances.*.private_ip[5]}\n[CART]\n${aws_instance.instances.*.private_ip[4]}\n[REDIS]\n${aws_instance.instances.*.private_ip[3]}\n[RABBITMQ]\n${aws_instance.instances.*.private_ip[2]}\n[MONGODB]\n${aws_instance.instances.*.private_ip[1]}\n[MYSQL]\n${aws_instance.instances.*.private_ip[0]}\n"
+//  filename    = "/tmp/inv-roboshop-${var.ENV}"
+//}
+
+locals {
+  COMPONENTS = concat(aws_instance.db-instances.*.private_ip, aws_instance.app-instances.*.private_ip)
+}
+
+resource "local_file" "inventory-file" {
+  content     = "[FRONTEND]\n${local.COMPONENTS[9]}\n[PAYMENT]\n${local.COMPONENTS[8]}\n[SHIPPING]\n${local.COMPONENTS[7]}\n[USER]\n${local.COMPONENTS[6]}\n[CATALOGUE]\n${local.COMPONENTS[5]}\n[CART]\n${local.COMPONENTS[4]}\n[REDIS]\n${local.COMPONENTS[3]}\n[RABBITMQ]\n${local.COMPONENTS[2]}\n[MONGODB]\n${local.COMPONENTS[1]}\n[MYSQL]\n${local.COMPONENTS[0]}\n"
+  filename    = "/tmp/inv-roboshop-${var.ENV}"
+}
