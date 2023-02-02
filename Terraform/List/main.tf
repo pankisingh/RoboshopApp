64f9f43e9059c2b40b21ab97355a929bb77c0d0e
Pankaj Singh <47411477+pankisingh@users.noreply.github.com>
2023-02-02 23:17:03 +0530

    Update main.tf

--- Terraform/List/main.tf
+++ Terraform/List/main.tf
@@ -1,9 +1,9 @@
-resource "random_pet" "my-list" {
+resource "random_name" "my-list" {
  prefix  = var.prefix[0]
 }
 
 output "my-Name" {
-    value = random_pet.my-list.prefix
+    value = random_name.my-list.prefix
 }
 
 output "name"{
